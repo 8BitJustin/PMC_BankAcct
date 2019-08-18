5bad0e6cb967457923508a164f2b96504b4a79f5
8bitjustin <j.olson.digital@gmail.com>
2019-08-18 16:51:29 -0700

    Altered file, added jack and john files. Jack owes bank money.

--- acc.py
+++ acc.py
@@ -18,17 +18,41 @@ class Account:
             file.write(str(self.balance))
 
 
-account = Account("balance.txt")
+class Checking(Account):
+
+    type = "checking"
+
+    def __init__(self, filepath, fee):
+        Account.__init__(self, filepath)
+        self.fee = fee
+
+    def transfer(self, amount):
+        self.balance = self.balance - amount - self.fee
+
+
+jack_checking = Checking("jack.txt", 1)
+jack_checking.transfer(100)
+print(jack_checking.balance)
+jack_checking.commit()
+
+john_checking = Checking("john.txt", 1)
+john_checking.transfer(100)
+print(john_checking.balance)
+john_checking.commit()
+
+
+# account = Account("balance.txt")
 
 # Prints balance text of balance.txt file
 # print(account.balance)
 
 # Runs withdraw method
-account.withdraw(100)
-print(account.balance)
+# account.withdraw(100)
+# print(account.balance)
 
 # Runs deposit method
 # account.deposit(1000)
 # print(account.balance)
 
-account.commit()
+# account.commit()
+
